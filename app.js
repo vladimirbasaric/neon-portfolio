042b4fab777a1feee660d47f5c90b8f562ecc1da
vladimirbasaric <vladimirbasaric@gmail.com>
2020-05-28 00:26:54 +0200

    active nav

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ let btns = navContainer.getElementsByClassName("nav-item");
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("glow");
-    current[0].className = current[0].className.replace(" glow", "");
-    this.className += " glow";
+    var current = document.getElementsByClassName("active glow");
+    current[0].className = current[0].className.replace(" active glow", "");
+    this.className += " active glow";
   });
 }
+
 
